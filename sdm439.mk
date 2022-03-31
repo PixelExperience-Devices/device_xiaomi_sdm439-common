8c96824f2c3eff6eac328bbf4e51d9ea4e9edaa3
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-31 20:37:52 +0530

    sdm439-common: permissions: Enable Google Dialer call recording feature
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- sdm439.mk
+++ sdm439.mk
@@ -225,6 +225,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
 
+# Google Dialer Call recording
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Google LILY_EXPERIENCE feature
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
