d53e5399916a12ce2727b6d2d45da81dbf6d9b77
ZeeLog <Danjel.Zammit@gmail.com>
2022-09-02 17:08:27 +0200

    sdm439-common: Update DRM Widevine from Coral
    * from google/coral/coral:Tiramisu/TPP1.220114.016/8158200:user/release-keys
    
    [shield44 - Stop building DRM 1.3 vendor HAL variant]
    
    Signed-off-by: shield44 <shield44@pixelexperience.org>
    Change-Id: Ie070c41451c26d95e1fad803c5d1357f2576af59

--- sdm439.mk
+++ sdm439.mk
@@ -183,9 +183,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
